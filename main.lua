bc816c9e562a4bc4bc6a979ae0150ec283dc737e
Gurcan Gulec <gurcanglc@gmail.com>
2017-05-13 13:39:40 +0300

    making a few changes to config.lua and main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,15 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+display.setStatusBar( display.HiddenStatusBar )
 
--- Your code here
+_SCREEN = {
+	width = display.contentWidth,
+	height = display.contentHeight
+}
+
+_SCREEN.CENTER = {
+	x = display.contentCenterX,
+	y = display.contentCenterY
+}
+
+local image = display.newImageRect("images/background.png", 1920, 1080)
+image.x = _SCREEN.CENTER.x
+image.y = _SCREEN.CENTER.y
