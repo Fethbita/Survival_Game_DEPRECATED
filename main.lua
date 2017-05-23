aca7a0b418a49cf67e6cd60ce6276f68b9db3662
Gurcan Gulec <gurcanglc@gmail.com>
2017-05-24 00:37:30 +0300

    Making changes to main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,7 @@
 display.setStatusBar( display.HiddenStatusBar )
 
+local widget = require( "widget" )
+
 _SCREEN = {
 	width = display.contentWidth,
 	height = display.contentHeight
@@ -13,3 +15,14 @@ _SCREEN.CENTER = {
 local image = display.newImageRect("images/background.png", 1920, 1080)
 image.x = _SCREEN.CENTER.x
 image.y = _SCREEN.CENTER.y
+
+local exploreButton = widget.newButton{
+	left = 150,
+	top = 200,
+	width = 350,
+	height = 150,
+	defaultFile = "exploreButton.png",
+	overFile = "exploreButtonOver.png",
+	label = "Explore Button",
+	onEvent = handleButtonEvent
+}
