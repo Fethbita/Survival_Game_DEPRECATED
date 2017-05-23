ebe609631852a3b8128e61713125ea3518afa3e6
Burak Can <Fethbita@users.noreply.github.com>
2017-05-24 01:49:31 +0300

    Tabs to spaces

--- config.lua
+++ config.lua
@@ -2,15 +2,14 @@
 local aspect_ratio = display.pixelHeight / display.pixelWidth
 
 application = {
-   content = {
-      width = aspect_ratio > 1.5 and 320 or math.floor(480 / aspect_ratio),
-      height = aspect_ratio < 1.5 and 480 or math.floor(320 * aspect_ratio),
-      scale = "letterBox",
-      fps = 60,
-
-      imageSuffix = {
-         ["@2x"] = 1.5,
-         ["@4x"] = 3.0,
+  content = {
+    width = aspect_ratio > 1.5 and 320 or math.floor(480 / aspect_ratio),
+    height = aspect_ratio < 1.5 and 480 or math.floor(320 * aspect_ratio),
+    scale = "letterBox",
+    fps = 60,
+    imageSuffix = {
+      ["@2x"] = 1.5,
+      ["@4x"] = 3.0,
       },
-   },
+    },
 }
--- main.lua
+++ main.lua
@@ -3,13 +3,13 @@ display.setStatusBar(display.HiddenStatusBar)
 local widget = require("widget")
 
 _SCREEN = {
-	width = display.contentWidth,
-	height = display.contentHeight
+  width = display.contentWidth,
+  height = display.contentHeight
 }
 
 _SCREEN.CENTER = {
-	x = display.contentCenterX,
-	y = display.contentCenterY
+  x = display.contentCenterX,
+  y = display.contentCenterY
 }
 
 local image = display.newImageRect("images/background.png", 1920, 1080)
@@ -17,21 +17,21 @@ image.x = _SCREEN.CENTER.x
 image.y = _SCREEN.CENTER.y
 
 local explore_button = widget.newButton{
-	left = 20,
-	top = 400,
-	width = 127,
-	height = 40,
-	defaultFile = "images/explore_button.png",
-	overFile = "images/explore_button_over.png",
-	onEvent = handleButtonEvent
+  left = 20,
+  top = 400,
+  width = 127,
+  height = 40,
+  defaultFile = "images/explore_button.png",
+  overFile = "images/explore_button_over.png",
+  onEvent = handleButtonEvent
 }
 
 local pickup_button = widget.newButton{
-	left = 180,
-	top = 400,
-	width = 127,
-	height = 40,
-	defaultFile = "images/pickup_button.png",
-	overFile = "images/pickup_button_over.png",
-	onEvent = handleButtonEvent
+  left = 180,
+  top = 400,
+  width = 127,
+  height = 40,
+  defaultFile = "images/pickup_button.png",
+  overFile = "images/pickup_button_over.png",
+  onEvent = handleButtonEvent
 }
