c6f7003cd07d13453315b31aa0e60b23b416a6cc
Burak Can <Fethbita@users.noreply.github.com>
2017-05-26 04:15:04 +0300

    Edited button click functions

--- main.lua
+++ main.lua
@@ -17,6 +17,12 @@ local image = display.newImageRect("images/background.png", 1920, 1080);
 image.x = _SCREEN.CENTER.x - 15;
 image.y = _SCREEN.CENTER.y + 50;
 
+local healthBar = display.newRoundedRect(160, 3, 320, 6, 3);
+healthBar:setFillColor(104/255, 159/255, 56/255);
+
+local energyBar = display.newRoundedRect(160, 9, 320, 6, 3);
+energyBar:setFillColor(2/255, 136/255, 209/255);
+
 local time_text = display.newText(mechanics.time_text[mechanics.time + 1], 0, 0, "Bellota-Regular", 24);
 time_text.x = _SCREEN.CENTER.x - 90;
 time_text.y = _SCREEN.CENTER.y - 200;
@@ -25,69 +31,151 @@ local logs_text = display.newText("Logs", 0,0, "Bellota-Regular", 24);
 logs_text.x = _SCREEN.CENTER.x;
 logs_text.y = _SCREEN.CENTER.y - 100;
 
-healthBar = display.newRoundedRect(160, 3, 320, 6, 3)
-healthBar:setFillColor(104/255, 159/255, 56/255)
+local explore_button, explore_button_pressed, pickup_button, pickup_button_pressed
+local build_button, build_button_pressed, mine_button, mine_button_pressed, rest_button, rest_button_pressed
 
-energyBar = display.newRoundedRect(160, 9, 320, 6, 3)
-energyBar:setFillColor(2/255, 136/255, 209/255)
-
-local function handleExploreEvent(event)
-  --if (event.phase == "ended") then
-    if (mechanics.energy >= 100) then
-      mechanics.energy = mechanics.energy - 100;
-      mechanics.updateBars(healthBar, energyBar);
-
-      mechanics.selected_buttons = mechanics.selected_buttons + 1;
-      if (mechanics.selected_buttons == 2) then
-        mechanics.pass_time();
-        time_text.text = mechanics.time_text[mechanics.time + 1];
-      end
-    end
-  --end
-end
-
-local function handlePickupEvent(event)
+function handleExploreEvent(event)
   if (event.phase == "ended") then
-    if (mechanics.energy >= 100) then
-      mechanics.energy = mechanics.energy - 100;
-      mechanics.updateBars(healthBar, energyBar);
-
+    if (mechanics.explore_button_isPressed) then
+      mechanics.explore_button_isPressed = false;
+      explore_button.isVisible = true;
+      explore_button_pressed.isVisible = false;
+      mechanics.selected_buttons = mechanics.selected_buttons - 1;
+    else
+      mechanics.explore_button_isPressed = true;
+      explore_button.isVisible = false;
+      explore_button_pressed.isVisible = true;
       mechanics.selected_buttons = mechanics.selected_buttons + 1;
-      if (mechanics.selected_buttons == 2) then
-        mechanics.pass_time();
-        time_text.text = mechanics.time_text[mechanics.time + 1];
+
+      if (mechanics.energy < 100) then
+        mechanics.explore_button_isPressed = false;
+        explore_button.isVisible = true;
+        explore_button_pressed.isVisible = false;
+        mechanics.selected_buttons = mechanics.selected_buttons - 1;
+        return;
       end
-      if (mechanics.time == 1) then
-        mechanics.pickup(1, 5);
-      else
-        mechanics.pickup(0, 3, 3);
+
+      if (mechanics.selected_buttons == 2) then
+        mechanics.pass_time(healthBar, energyBar, time_text, explore_button, explore_button_pressed, pickup_button, pickup_button_pressed, build_button, build_button_pressed, mine_button, mine_button_pressed, rest_button, rest_button_pressed);
       end
     end
   end
 end
 
-
-local function handleRestEvent(event)
+function handlePickupEvent(event)
   if (event.phase == "ended") then
+    if (mechanics.pickup_button_isPressed) then
+      mechanics.pickup_button_isPressed = false;
+      pickup_button.isVisible = true;
+      pickup_button_pressed.isVisible = false;
+      mechanics.selected_buttons = mechanics.selected_buttons - 1;
+    else
+      mechanics.pickup_button_isPressed = true;
+      pickup_button.isVisible = false;
+      pickup_button_pressed.isVisible = true;
+      mechanics.selected_buttons = mechanics.selected_buttons + 1;
+
+      if (mechanics.energy < 100) then
+        mechanics.pickup_button_isPressed = false;
+        pickup_button.isVisible = true;
+        pickup_button_pressed.isVisible = false;
+        mechanics.selected_buttons = mechanics.selected_buttons - 1;
+        return;
+      end
+
+      if (mechanics.selected_buttons == 2) then
+        mechanics.pass_time(healthBar, energyBar, time_text, explore_button, explore_button_pressed, pickup_button, pickup_button_pressed, build_button, build_button_pressed, mine_button, mine_button_pressed, rest_button, rest_button_pressed);
+      end
+
+    end
   end
 end
 
-local function handleBuildEvent(event)
+function handleRestEvent(event)
   if (event.phase == "ended") then
+    if (mechanics.rest_button_isPressed) then
+      mechanics.rest_button_isPressed = false;
+      rest_button.isVisible = true;
+      rest_button_pressed.isVisible = false;
+      mechanics.selected_buttons = mechanics.selected_buttons - 1;
+    else
+      mechanics.rest_button_isPressed = true;
+      rest_button.isVisible = false;
+      rest_button_pressed.isVisible = true;
+      mechanics.selected_buttons = mechanics.selected_buttons + 1;
+
+      if (mechanics.selected_buttons == 2) then
+        mechanics.pass_time(healthBar, energyBar, time_text, explore_button, explore_button_pressed, pickup_button, pickup_button_pressed, build_button, build_button_pressed, mine_button, mine_button_pressed, rest_button, rest_button_pressed);
+      end
+
+    end
   end
 end
 
-local function handleMineEvent(event)
+function handleBuildEvent(event)
   if (event.phase == "ended") then
+    if (mechanics.build_button_isPressed) then
+      mechanics.build_button_isPressed = false;
+      build_button.isVisible = true;
+      build_button_pressed.isVisible = false;
+      mechanics.selected_buttons = mechanics.selected_buttons - 1;
+    else
+      mechanics.build_button_isPressed = true;
+      build_button.isVisible = false;
+      build_button_pressed.isVisible = true;
+      mechanics.selected_buttons = mechanics.selected_buttons + 1;
+
+      if (mechanics.energy < 100) then
+        mechanics.build_button_isPressed = false;
+        build_button.isVisible = true;
+        build_button_pressed.isVisible = false;
+        mechanics.selected_buttons = mechanics.selected_buttons - 1;
+        return;
+      end
+
+      if (mechanics.selected_buttons == 2) then
+        mechanics.pass_time(healthBar, energyBar, time_text, explore_button, explore_button_pressed, pickup_button, pickup_button_pressed, build_button, build_button_pressed, mine_button, mine_button_pressed, rest_button, rest_button_pressed);
+      end
+
+    end
+  end
+end
+
+function handleMineEvent(event)
+  if (event.phase == "ended") then
+    if (mechanics.mine_button_isPressed) then
+      mechanics.mine_button_isPressed = false;
+      mine_button.isVisible = true;
+      mine_button_pressed.isVisible = false;
+      mechanics.selected_buttons = mechanics.selected_buttons - 1;
+    else
+      mechanics.mine_button_isPressed = true;
+      mine_button.isVisible = false;
+      mine_button_pressed.isVisible = true;
+      mechanics.selected_buttons = mechanics.selected_buttons + 1;
+
+      if (mechanics.energy < 100) then
+        mechanics.mine_button_isPressed = false;
+        mine_button.isVisible = true;
+        mine_button_pressed.isVisible = false;
+        mechanics.selected_buttons = mechanics.selected_buttons - 1;
+        return;
+      end
+
+      if (mechanics.selected_buttons == 2) then
+        mechanics.pass_time(healthBar, energyBar, time_text, explore_button, explore_button_pressed, pickup_button, pickup_button_pressed, build_button, build_button_pressed, mine_button, mine_button_pressed, rest_button, rest_button_pressed);
+      end
+
+    end
   end
 end
 
 -- Creating explore button
-local explore_button = display.newImageRect("images/button.png", 140, 52);
-explore_button:addEventListener("tap", handleExploreEvent);
+explore_button = display.newImageRect("images/button.png", 140, 52);
+explore_button:addEventListener("touch", handleExploreEvent);
 
-local explore_button_pressed = display.newImageRect("images/button_over.png", 140, 52);
-explore_button_pressed:addEventListener("tap", handleExploreEvent);
+explore_button_pressed = display.newImageRect("images/button_over.png", 140, 52);
+explore_button_pressed:addEventListener("touch", handleExploreEvent);
 explore_button_pressed.isVisible = false;
 
 local explore_button_text = display.newText( "Explore", 0, 0, "Bellota-Regular", 24 );
@@ -100,10 +188,10 @@ explore_button_group.x = _SCREEN.CENTER.x - 80
 explore_button_group.y = _SCREEN.CENTER.y + 90
 
 -- Creating rest button
-local rest_button = display.newImageRect("images/button.png", 140, 52);
+rest_button = display.newImageRect("images/button.png", 140, 52);
 rest_button:addEventListener("touch", handleRestEvent);
 
-local rest_button_pressed = display.newImageRect("images/button_over.png", 140, 52);
+rest_button_pressed = display.newImageRect("images/button_over.png", 140, 52);
 rest_button_pressed:addEventListener("touch", handleRestEvent);
 rest_button_pressed.isVisible = false;
 
@@ -117,10 +205,10 @@ rest_button_group.x = _SCREEN.CENTER.x - 80
 rest_button_group.y = _SCREEN.CENTER.y + 140
 
 -- Creating build button
-local build_button = display.newImageRect("images/button.png", 140, 52);
+build_button = display.newImageRect("images/button.png", 140, 52);
 build_button:addEventListener("touch", handleBuildEvent);
 
-local build_button_pressed = display.newImageRect("images/button_over.png", 140, 52);
+build_button_pressed = display.newImageRect("images/button_over.png", 140, 52);
 build_button_pressed:addEventListener("touch", handleBuildEvent);
 build_button_pressed.isVisible = false;
 
@@ -134,10 +222,10 @@ build_button_group.x = _SCREEN.CENTER.x - 80
 build_button_group.y = _SCREEN.CENTER.y + 190
 
 -- Creating pick up button
-local pickup_button = display.newImageRect("images/button.png", 140, 52);
+pickup_button = display.newImageRect("images/button.png", 140, 52);
 pickup_button:addEventListener("touch", handlePickupEvent);
 
-local pickup_button_pressed = display.newImageRect("images/button_over.png", 140, 52);
+pickup_button_pressed = display.newImageRect("images/button_over.png", 140, 52);
 pickup_button_pressed:addEventListener("touch", handlePickupEvent);
 pickup_button_pressed.isVisible = false;
 
@@ -151,10 +239,10 @@ pickup_button_group.x = _SCREEN.CENTER.x + 80
 pickup_button_group.y = _SCREEN.CENTER.y + 190
 
 -- Creating mine button
-local mine_button = display.newImageRect("images/button.png", 140, 52);
+mine_button = display.newImageRect("images/button.png", 140, 52);
 mine_button:addEventListener("touch", handleMineEvent);
 
-local mine_button_pressed = display.newImageRect("images/button_over.png", 140, 52);
+mine_button_pressed = display.newImageRect("images/button_over.png", 140, 52);
 mine_button_pressed:addEventListener("touch", handleMineEvent);
 mine_button_pressed.isVisible = false;
 
--- mechanics.lua
+++ mechanics.lua
@@ -1,33 +1,87 @@
 local mechanics = {};
 
-local health = 1000;
-mechanics.health = health;
-local energy = 1000;
-mechanics.energy = energy;
-local inventory = {
+mechanics.health = 1000;
+mechanics.energy = 1000;
+mechanics.inventory = {
   Wood = 0;
   Stone = 0;
   Berry = 0;
   Branch = 0;
 };
-mechanics.inventory = inventory;
 --[[
 0 means nigttime
 1 means daytime
 --]]
-local time = 1;
-mechanics.time = time;
-local time_text = {"Nighttime", "Daytime"};
-mechanics.time_text = time_text;
+mechanics.time = 1;
+mechanics.time_text = {"Nighttime", "Daytime"};
 
+mechanics.explore_button_isPressed = false;
+mechanics.pickup_button_isPressed = false;
+mechanics.rest_button_isPressed = false;
+mechanics.build_button_isPressed = false;
+mechanics.mine_button_isPressed = false;
 
-local selected_buttons = 0;
-mechanics.selected_buttons = selected_buttons;
+mechanics.selected_buttons = 0;
 
-
-function mechanics.pass_time()
+function mechanics.pass_time(healthBar, energyBar, time_text, explore_button, explore_button_pressed, pickup_button, pickup_button_pressed, build_button, build_button_pressed, mine_button, mine_button_pressed, rest_button, rest_button_pressed)
   mechanics.time = 1 - mechanics.time;
   mechanics.selected_buttons = 0;
+
+  time_text.text = mechanics.time_text[mechanics.time + 1];
+
+  if (explore_button_pressed.isVisible) then
+
+    mechanics.energy = mechanics.energy - 100;
+    mechanics.updateBars(healthBar, energyBar);
+
+    mechanics.explore_button_isPressed = false;
+    explore_button.isVisible = true;
+    explore_button_pressed.isVisible = false;
+  end
+
+  if (pickup_button_pressed.isVisible) then
+
+    mechanics.energy = mechanics.energy - 100;
+    mechanics.updateBars(healthBar, energyBar);
+    if (mechanics.time == 1) then
+      mechanics.pickup(1, 5);
+    else
+      mechanics.pickup(0, 3, 3);
+    end
+
+    mechanics.pickup_button_isPressed = false;
+    pickup_button.isVisible = true;
+    pickup_button_pressed.isVisible = false;
+  end
+
+  if (rest_button_pressed.isVisible) then
+
+    mechanics.updateBars(healthBar, energyBar);
+
+    mechanics.rest_button_isPressed = false;
+    rest_button.isVisible = true;
+    rest_button_pressed.isVisible = false;
+  end
+
+  if (build_button_pressed.isVisible) then
+
+    mechanics.energy = mechanics.energy - 100;
+    mechanics.updateBars(healthBar, energyBar);
+
+    mechanics.build_button_isPressed = false;
+    build_button.isVisible = true;
+    build_button_pressed.isVisible = false;
+  end
+
+  if (mine_button_pressed.isVisible) then
+
+    mechanics.energy = mechanics.energy - 100;
+    mechanics.updateBars(healthBar, energyBar);
+
+    mechanics.mine_button_isPressed = false;
+    mine_button.isVisible = true;
+    mine_button_pressed.isVisible = false;
+  end
 end
 
 function mechanics.explore()
@@ -41,14 +95,14 @@ then with the prob_power given, creates a distribution.
 --]]
 function mechanics.pickup(min_object, max_object, prob_power)
   prob_power = prob_power or 4
-  for key,value in pairs(inventory) do
+  for key,value in pairs(mechanics.inventory) do
     number_found = math.floor(min_object + ((max_object + 1) - min_object) * math.random() ^ prob_power);
-    inventory[key] = inventory[key] + number_found;
+    mechanics.inventory[key] = mechanics.inventory[key] + number_found;
   end
-  print("Wood " .. inventory.Wood .. "   ");
-  print("Stone " .. inventory.Stone .. "   ");
-  print("Berry " .. inventory.Berry .. "   ");
-  print("Branch " .. inventory.Branch .. "   ");
+  print("Wood " .. mechanics.inventory.Wood .. "   ");
+  print("Stone " .. mechanics.inventory.Stone .. "   ");
+  print("Berry " .. mechanics.inventory.Berry .. "   ");
+  print("Branch " .. mechanics.inventory.Branch .. "   ");
 end
 
 function mechanics.updateBars(healthBar, energyBar)
